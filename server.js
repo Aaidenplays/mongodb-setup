c98b3b7a17484c0dd1a05f823161fdec171a06de
aaidenplays <aaidenplays@yahoo.com>
2020-09-16 09:59:58 -0500

    database is connected

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+
+//db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('DB Connected'));
+
+//error catching
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+
 app.listen(8080, () => {
     console.log(`A Node JS API is listening`);
 });
+
