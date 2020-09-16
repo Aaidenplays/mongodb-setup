cb7560aced0aded3b0209aef4e19d34b72712809
aaidenplays <aaidenplays@yahoo.com>
2020-09-16 10:12:45 -0500

    added missing packages

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config()
 
 //db
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
